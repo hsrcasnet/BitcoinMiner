44bf6f201c5ee7835ee1c401ce7c894f94a5f9d0
Thomas Galliker <thomas@bluewin.ch>
2019-09-09 08:15:38 +0200

    Install analysers

---
+++ BitcoinMiner.Console/WorkState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitcoinMiner.Console
+{
+    public enum WorkState : long // CA1028
+    {
+        Idle = 2,
+        InProgress = 3,
+    }
+}
