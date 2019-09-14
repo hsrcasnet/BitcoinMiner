90ad23634f8a6a50edf37288e38e9af9732b0d57
Thomas Galliker <thomas@bluewin.ch>
2019-09-14 08:25:04 +0200

    Add depricated method

--- BitcoinMiner.Console/BitCoinWebClient.cs
+++ BitcoinMiner.Console/BitCoinWebClient.cs
@@ -12,7 +12,17 @@ namespace BitcoinMiner.Console
         /// <summary>
         /// Simulates a Peer-to-Peer Bitcoin Pool.
         /// </summary>
+        [Obsolete("Will be depricated by version 2.x, please use Request() method")]
         internal string Request(string method, string paramString)
+        {
+            var generatedData = GenerateRandomHex(200);
+            return $"{{\"id\":3,\"result\":{method},\"paramString\":{paramString},\"error\":null, \"data\": \"{generatedData}\"}}";
+        }
+
+        /// <summary>
+        /// Simulates a Peer-to-Peer Bitcoin Pool.
+        /// </summary>
+        internal string Request()
         {
             var generatedData = GenerateRandomHex(200);
             return $"{{\"id\":3,\"result\":true,\"error\":null, \"data\": \"{generatedData}\"}}";
