827f3a27d1235476ae8c3e0daff88f7b22f83a3d
Thomas Galliker <thomas@bluewin.ch>
2019-09-14 08:25:11 +0200

    Add code suppression

--- BitcoinMiner.Console/GlobalSuppressions.cs
+++ BitcoinMiner.Console/GlobalSuppressions.cs
@@ -1,4 +1,5 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage 
+﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification = "<Pending>", Scope = "member", Target = "~M:MiniMiner.Work.FindShare(System.UInt32@,System.UInt32)~System.Boolean")]
+// This file is used by Code Analysis to maintain SuppressMessage 
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
