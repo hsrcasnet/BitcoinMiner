613877095b148e38aaafa6e79e48d6ab12414512
Thomas Galliker <thomas@bluewin.ch>
2021-09-08 17:12:27 +0200

    Add more bad code

---
+++ BitcoinMiner.Console/Program2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitcoinMiner.Console
+{
+    internal class Program2
+    {
+        static void Main2(string[] args)
+        {
+        }
+
+        int Add(int i1, int i2)
+        {
+            return i1 + i2;
+        }
+    }
+}
