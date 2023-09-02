018fb01e8e23528caf4a99acb634264be174b270
Thomas Galliker <thomas@bluewin.ch>
2023-09-02 15:31:52 +0200

    Cleanup

---
+++ BitcoinMiner.Console/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace BitcoinMiner.Console
+{
+    // TODO: [work item 33333] Remove unused code 
+    public class Calculator
+    {
+        public int Add(int i1, int i2)
+        {
+            return i1 + i2;
+        }
+    }
+}
